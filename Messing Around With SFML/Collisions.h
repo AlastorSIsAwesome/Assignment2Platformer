942083e74adb37669484303d104d5d82e857e006
Alastor Spear <alastor.spear@mds.ac.nz>
2026-08-26 22:04:42 +1200

    collisions work!!

--- Messing Around With SFML/Collisions.h
+++ Messing Around With SFML/Collisions.h
@@ -1,21 +1,52 @@
 #pragma once
-#include <SFML/Graphics.hpp>
+#include "SFML/Graphics.hpp"
 
 static class Collisions
 {
-private:
-protected:
 public:
-	static void ResolveXCollisions(sf::Shape* _objA, sf::FloatRect* _objB, bool SecondObjectWall)
+	static void ResolveXCollisions(sf::Shape* _objA, sf::Shape* _objB, bool _secondObjectWall) // bool is here if you want collision to react differently if the second object is a wall (or something else)
 	{
-		sf::Vector2f entityACenter = 
+		// global bounds == size and position with respect to the 2D world and any transformations done to the object
+		sf::Vector2f entityACenter = { (_objA->getGlobalBounds().position.x + (_objA->getGlobalBounds().size.x / 2.0f)), // .position returns the location of the top left corner, add half of the object's size and you get the center of the object!
+									  (_objA->getGlobalBounds().position.y + (_objA->getGlobalBounds().size.y / 2.0f)) };
+
+		sf::Vector2f entityBCenter = { (_objB->getGlobalBounds().position.x + (_objB->getGlobalBounds().size.x / 2.0f)), // same as for A
+									  (_objB->getGlobalBounds().position.y + (_objB->getGlobalBounds().size.y / 2.0f)) };
+
+		if (entityACenter.x <= entityBCenter.x) // if A is to the left of B
 		{
-			(_objA->getGlobalBounds().position.x
-		};
+			float offset = -(_objA->getGlobalBounds().position.x + _objA->getGlobalBounds().size.x - _objB->getGlobalBounds().position.x); // don't need to half anything bc its raw additon & subtraction
+			_objA->move({ offset, 0 }); // moving A back to where it was before colliding
+		}
+
+		if (entityACenter.x >= entityBCenter.x) // if A is to the right of B
+		{
+			float offset = (_objB->getGlobalBounds().position.x + _objB->getGlobalBounds().size.x) - _objA->getGlobalBounds().position.x;
+			_objA->move(sf::Vector2f(offset, 0)); // moving A back to where it was before colliding
+		}
 	}
+	static void ResolveYCollisions(sf::Shape* _objA, sf::Shape* _objB, bool _secondObjectWall)
+	{
+		sf::Vector2f entityACenter = { (_objA->getGlobalBounds().position.x + _objA->getGlobalBounds().size.x / 2.0f),
+									  (_objA->getGlobalBounds().position.y + _objA->getGlobalBounds().size.y / 2.0f) };
 
+		sf::Vector2f entityBCenter = { (_objB->getGlobalBounds().position.x + _objB->getGlobalBounds().size.x / 2.0f),
+									  (_objB->getGlobalBounds().position.y + _objB->getGlobalBounds().size.y / 2.0f) };
 
+		// underneath the tile
+		if (entityACenter.y <= entityBCenter.y)
+		{
+			float offset = -(_objA->getGlobalBounds().position.y + _objA->getGlobalBounds().size.y - _objB->getGlobalBounds().position.y);
+			_objA->move({ 0, offset });
+		}
+
+		// above the tile. 
+		if (entityACenter.y >= entityBCenter.y)
+		{
+			float offset = (_objB->getGlobalBounds().position.y + _objB->getGlobalBounds().size.y) - _objA->getGlobalBounds().position.y;
+			_objA->move({ 0, offset });
+		}
+	}
 };
-
 
 
--- Messing Around With SFML/main.cpp
+++ Messing Around With SFML/main.cpp
@@ -1,18 +1,42 @@
 #include <SFML/Graphics.hpp>
+#include "Collisions.h"
+
+
+float UpdatePlayer(float _playerYVelocity, float _YVelocity, float _dt)
+{
+    if (_playerYVelocity < 4.f)
+    {
+        _playerYVelocity += _YVelocity * _dt * 2; // (in/de)creaces
+        return _playerYVelocity;
+    }
+}
+
+
+
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({ 1280, 720 }), "ALASTOR SPHERE RETURNS");
+
+    // create objects
     sf::RectangleShape Player({ 100.f, 100.f });
+    sf::RectangleShape Ground({ 100.f, 100.f });
+    sf::RectangleShape Ground1({ 100.f, 100.f });
+    sf::RectangleShape Ground2({ 100.f, 100.f });
     
+    Player.setPosition({ 300, 200 });
+    Ground.setPosition({ 300, 400 });
+    Ground1.setPosition({ 500, 400 });
+    Ground2.setPosition({ 700, 400 });
+
+
     // Texture Setting
     sf::Texture PlayerTexture;
     PlayerTexture.loadFromFile("textures/alastorsphere.png");
     Player.setTexture(&PlayerTexture);
 
-
-
-    sf::RenderTexture; 
+    float PlayerYVelocity = 0.0f;
+    float PlayerXVelocity = 0.0f;
 
     while (window.isOpen())
     {
@@ -20,12 +44,73 @@ int main()
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
-
-            
         }
 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::R))
+        {
+            Player.setPosition({ 300,200 });
+        }
+
+        PlayerXVelocity = 0.0f;
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::W))
+        {
+            PlayerYVelocity = -2;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::S))
+        {
+            PlayerYVelocity = 1;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::A))
+        {
+            PlayerXVelocity = -1;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::D))
+        {
+            PlayerXVelocity = 1;
+        }
+
+
         window.clear();
+
+
+        PlayerYVelocity = UpdatePlayer(PlayerYVelocity, 1.0f, 0.0167); 
+        
+        Player.move({ 0, PlayerYVelocity });
+        if (Player.getGlobalBounds().findIntersection(Ground.getGlobalBounds()))
+        {
+            Collisions::ResolveYCollisions(&Player, &Ground, 1);
+        }
+        else if (Player.getGlobalBounds().findIntersection(Ground1.getGlobalBounds()))
+        {
+            Collisions::ResolveYCollisions(&Player, &Ground1, 1);
+        }
+        else if (Player.getGlobalBounds().findIntersection(Ground2.getGlobalBounds()))
+        {
+            Collisions::ResolveYCollisions(&Player, &Ground2, 1);
+        }
+
+        Player.move({ PlayerXVelocity, 0 });
+        if (Player.getGlobalBounds().findIntersection(Ground.getGlobalBounds()))
+        {
+            Collisions::ResolveXCollisions(&Player, &Ground, 1);
+        }
+        else if (Player.getGlobalBounds().findIntersection(Ground1.getGlobalBounds()))
+        {
+            Collisions::ResolveXCollisions(&Player, &Ground1, 1);
+        }
+        else if (Player.getGlobalBounds().findIntersection(Ground2.getGlobalBounds()))
+        {
+            Collisions::ResolveXCollisions(&Player, &Ground2, 1);
+        }
+
+
         window.draw(Player);
+        window.draw(Ground);
+        window.draw(Ground1);
+        window.draw(Ground2);
+
         window.display();
     }
+    return 0;
 }
