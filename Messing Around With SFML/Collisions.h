9e906c453e7a39114afadf5eadc3c197172a62ce
Alastor Adelaide Spear <Alastor.Spear@mds.ac.nz>
2026-08-26 17:26:48 +1200

    AHHHHHHHHHHHHHHHHH

---
+++ Messing Around With SFML/Collisions.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+static class Collisions
+{
+private:
+protected:
+public:
+	static void ResolveXCollisions(sf::Shape* _objA, sf::FloatRect* _objB, bool SecondObjectWall)
+	{
+		sf::Vector2f entityACenter = 
+		{
+			(_objA->getGlobalBounds().position.x
+		};
+	}
+
+
+};
+
+
+
--- Messing Around With SFML/main.cpp
+++ Messing Around With SFML/main.cpp
@@ -1,29 +1,15 @@
 #include <SFML/Graphics.hpp>
 
-
 int main()
 {
-
-
     sf::RenderWindow window(sf::VideoMode({ 1280, 720 }), "ALASTOR SPHERE RETURNS");
     sf::RectangleShape Player({ 100.f, 100.f });
     
     // Texture Setting
     sf::Texture PlayerTexture;
     PlayerTexture.loadFromFile("textures/alastorsphere.png");
-
-
     Player.setTexture(&PlayerTexture);
 
-    bool CurrentlyDrawing = false;
-    //bool CurrentlyResizing = false;
-
-    // pointer array to keep track of shapes
-   // sf::Shape* pShapeArray = new sf::Shape*[0];
-
-   // sf::Shape test[1] = ;
-
-    sf::Vector2f IncreaseScale(1.f, 1.f);
 
 
     sf::RenderTexture; 
@@ -34,8 +20,9 @@ int main()
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
-        }
 
+            
+        }
 
         window.clear();
         window.draw(Player);
